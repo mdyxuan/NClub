5d87119b2573c693b99b4a722df7f24baec8f4be
mdyxuan <emily450033@gmail.com>
2024-11-02 19:25:41 +0800

    2024-11-02

---
+++ Application3.1/app/src/main/java/com/example/application/Activity/NotificationActivity.java
@@ -0,0 +1,23 @@
+package com.example.application.Activity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.application.Adapter.CustomAdapter;
+import com.example.application.R;
+
+public class NotificationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        ListView listView = findViewById(R.id.listview);
+
+        // 示例數據
+        String[] data = {"項目 1", "項目 2", "項目 3"};
+
+        // 創建自定義適配器並設置給 ListView
+        CustomAdapter adapter = new CustomAdapter(this, data);
+        listView.setAdapter(adapter);
+    }
+}
